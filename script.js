5882969a149961cb61a213e19acf3f8dbfaa4613
MFRodrigu3s <m.f3rreirar@gmail.com>
2023-02-15 16:19:47 -0300

    tudo quase pronto

---
+++ script.js
@@ -0,0 +1,24 @@
+const input = document.getElementById('input')
+const botao = document.getElementById('botao')
+const portaAberta = document.getElementById('portaAberta')
+const portaFechada = document.getElementById('portaFechada')
+const subtitulo = document.getElementById('subtitulo')
+const msgAmor = document.getElementById('msgAmor')
+const msgErro = document.getElementById('msgErro')
+
+botao.onclick = function(){
+    if(input.value != "BRUNA CICILIA PRESTES"){
+        portaFechada.style.display = "block"
+        portaAberta.style.display = "none"
+        msgErro.style.display = "block"
+        subtitulo.style.display = "none"
+        msgAmor.style.display = "none"
+    } else {
+        // alert('deu certo')
+        portaFechada.style.display = "none"
+        portaAberta.style.display = "block"
+        msgErro.style.display = "none"
+        msgAmor.style.display = "block"
+        subtitulo.style.display = "none"
+    }
+}    
